bf0de31c1a67a316ea8b632d6203031073caaad9
EA <arrive@free.fr>
2023-04-06 22:12:21 +0200

    add page home and footer

---
+++ templates/portal/footer.php
@@ -0,0 +1,55 @@
+<footer class="footer" role="contentinfo">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<p>
+					<a href="https://www.cercleferdinanddesaussure.org/">
+						<img src="/public/site/images/ojsadmin/CFS_logo.png">
+					</a>
+				</p>
+				<p>
+					Cercle Ferdinand de Saussure<br>
+					Anne-Gaëlle TOUTAIN<br>
+					Institut de langue et de littérature françaises de l’Université de Berne<br>
+					Unitobler<br>
+					Länggassstrasse, 49<br>
+					CH-3012 Berne<br>
+					Suisse<br>
+					<a href="mailto:comite@cercleferdinanddesaussure.org">comite@cercleferdinanddesaussure.org</a>
+				</p>
+				<p>
+					<img src="/public/site/images/ojsadmin/CFS_ASSH.png" width="125" height="52">
+				</p>
+				ISSN numérique: 2673-7329<br />
+				ISSN: 0068-516X
+			</div>
+			<div class="col-md-4">
+				<p>
+					<a href="/CFS/comite-editorial"> Comité éditorial </a>
+				</p>
+				<p>
+					<a href="/CFS/about/contact"> Contact </a>
+				</p>
+				<p>
+					<a href="/CFS/about"> À propos de la revue </a>
+				</p>
+				<p>
+					<a href="/CFS/licence"> Mentions légales </a>
+				</p>
+			</div>
+			<div class="col-md-4">
+				<img src="/public/site/logo-droz-77x80.png" />
+				<p>
+					Librairie Droz S.A.<br/ >
+					11, rue Firmin-Massot<br/ >
+					B.P. 389<br/ >
+					CH – 1211 Genève 12<br/ >
+					Téléphone : +41 22 346 66 66<br/ >
+					Télécopie : +41 22 347 23 91<br/ >
+					<a target="_blank" href="https://www.droz.org">https://www.droz.org</a>
+				</p>
+				common.openJournalSystems
+			</div>
+		</div>
+	</div>
+</footer>
