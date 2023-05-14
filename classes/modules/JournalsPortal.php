73c5feae4b06e962e6f09dcb5ad239cd0539126b
EA <arrive@free.fr>
2023-05-14 22:29:53 +0200

    cosmetic

--- classes/modules/JournalsPortal.php
+++ classes/modules/JournalsPortal.php
@@ -275,7 +275,7 @@ class JournalsPortal extends Portal {
             'id'              => $short,
             'title'           => $_paper['title'],
             'volume'          => $issue->volume,
-            'container-title' => strip_tags($_journal['name']),
+            'container-title' => str_replace('<br/>', ' ', $_journal['name']),
             'page'            => JournalsUtils::pages($paper),
             'accessed'        => ["date-parts" => [[date('Y', $now), date('m', $now), date('d', $now)]]],
             'issued'          => ["date-parts" => [[date('Y', $issued), date('m', $issued), date('d', $issued)]]]
