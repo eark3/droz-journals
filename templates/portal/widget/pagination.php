76995a7e3d794436b675222c2518e14f59a6363d
EA <arrive@free.fr>
2023-05-24 22:32:52 +0200

    cosmetic

--- templates/portal/widget/pagination.php
+++ templates/portal/widget/pagination.php
@@ -4,7 +4,7 @@
 				<span data-search="<?php echo $models['search'] ?? ''; ?>" data-start="0">&lt;&lt;</span>&nbsp;
 				<span data-search="<?php echo $models['search'] ?? ''; ?>" data-start="<?php echo max([0, $start - $rows]); ?>">&lt;</span>&nbsp;
 <?php   for ($index = 0; $index < ceil($models['found'] / $models['rows']); $index++) { ?>
-				<span data-search="<?php echo $models['search'] ?? ''; ?>"<?php echo $models['start'] == $index * $models['rows'] ? ' style="font-weight:bold;"' : ''; ?> data-start="<?php echo $index * $models['rows']; ?>"><?php echo $index + 1; ?></span>&nbsp;
+				<span data-search="<?php echo $models['search'] ?? ''; ?>"<?php echo $models['start'] == $index * $models['rows'] ? ' style="font-weight:bold;border:1px solid black;"' : ''; ?> data-start="<?php echo $index * $models['rows']; ?>"><?php echo $index + 1; ?></span>&nbsp;
 <?php   } ?>
 				<span data-search="<?php echo $models['search'] ?? ''; ?>" data-start="<?php echo min([$models['rows'] * floor($models['found'] / $models['rows']), $start + $rows]); ?>">&gt;</span>&nbsp;
 				<span data-search="<?php echo $models['search'] ?? ''; ?>" data-start="<?php echo $models['rows'] * floor($models['found'] / $models['rows']); ?>">&gt;&gt;</span>&nbsp;
