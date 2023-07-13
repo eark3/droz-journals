a9408b961bfdb9799c78a7f8e8f6babc912b1722
EA <arrive@free.fr>
2023-07-13 13:10:36 +0200

    cosmetic

--- classes/process/JournalsImport.php
+++ classes/process/JournalsImport.php
@@ -309,7 +309,7 @@ class JournalsImport extends Import {
             return false;
         }
         $this->issue = Zord::arrayFromJSONFile($this->folder.$ean.'.json');
-        if (!is_array($this->issue) || !Zord::is_associative($this->issue)) {
+        if (file_exists($this->folder.$ean.'.json') && (!is_array($this->issue) || !Zord::is_associative($this->issue))) {
             $this->error(3, Zord::substitute($this->locale->messages->check->error->json->wrong, ['json' => $ean.'.json']));
             return false;
         }
