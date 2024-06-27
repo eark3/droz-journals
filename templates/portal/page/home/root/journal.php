a8f207840cb68e66920ab3950bb64aa87e410e8e
EA <arrive@free.fr>
2024-06-27 08:08:31 +0200

    cosmetic

--- templates/portal/page/home/root/journal.php
+++ templates/portal/page/home/root/journal.php
@@ -1,21 +1,21 @@
 					<li class="media panel panel-default" style="padding: 15px;">
  						<div class="media-left">
-							<a href="/<?php echo $journal['context']; ?>">
+							<a href="<?php echo Zord::getContextURL($journal['context']); ?>">
 								<img class="media-object" src="/public/journals/images/<?php echo $journal['context']; ?>/<?php echo $journal['settings']['homepageImage']['uploadName'] ?? 'homepageImage_fr_FR.jpg'; ?>">
 							</a>
 						</div>
 						<div class="media-body">
 							<h3 class="media-heading">
-								<a href="/<?php echo $journal['context']; ?>" rel="bookmark"><?php echo $journal['settings']['name'] ?? Zord::value('context', [$journal['context'],'title',$journal['locale']]); ?></a>
+								<a href="<?php echo Zord::getContextURL($journal['context']); ?>" rel="bookmark"><?php echo $journal['settings']['name'] ?? Zord::value('context', [$journal['context'],'title',$journal['locale']]); ?></a>
 							</h3>
 							<div class="description">
 								<?php echo $journal['settings']['rootDescription'] ?? ''; ?>
 							</div>
 							<ul class="nav nav-pills">
 								<li class="view">
-									<a href="/<?php echo $journal['context']; ?>"><?php echo Zord::getLocale('portal'.DS.$journal['context'], $lang)->root->journal ?? $locale->root->journal; ?></a></li>
+									<a href="<?php echo Zord::getContextURL($journal['context']); ?>"><?php echo Zord::getLocale('portal'.DS.$journal['context'], $lang)->root->journal ?? $locale->root->journal; ?></a></li>
 								<li class="current">
-									<a href="/<?php echo $journal['context']; ?>/issue/current"><?php echo Zord::getLocale('portal'.DS.$journal['context'], $lang)->root->current ?? $locale->root->current; ?></a></li>
+									<a href="/<?php echo Zord::getContextURL($journal['context']); ?>/issue/current"><?php echo Zord::getLocale('portal'.DS.$journal['context'], $lang)->root->current ?? $locale->root->current; ?></a></li>
 							</ul>
 						</div>
 					</li>
