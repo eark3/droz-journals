24b94e8be400fdb61954465186d398796a572df7
EA <arrive@free.fr>
2025-01-29 13:31:53 +0100

    cosmetic

--- templates/portal/footer.php
+++ templates/portal/footer.php
@@ -30,7 +30,6 @@
 					B.P. 389<br/ >
 					CH – 1211 Genève 12<br/ >
 					Téléphone : +41 22 346 66 66<br/ >
-					Télécopie : +41 22 347 23 91<br/ >
 					<a target="_blank" href="https://www.droz.org">https://www.droz.org</a>
 				</p>
 			</div>
