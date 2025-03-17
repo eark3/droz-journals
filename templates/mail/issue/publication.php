1b7a5736170dbbc0e55c7ed4df36b961b4c103b3
EA <arrive@free.fr>
2025-03-17 17:55:02 +0100

    cosmetic

--- templates/mail/issue/publication.php
+++ templates/mail/issue/publication.php
@@ -2,6 +2,6 @@
 <div class="content">
 	<p>Un nouveau numéro a été publié.</p>
 	<p>Vous pouvez le consulter en cliquant le lien ci dessous.</p>
-	<p><a href="<?php echo $baseURL; ?>/issue/current"><?php echo $baseURL; ?>/issue/current</a></p>
+	<p><a href="<?php echo $baseURL.'/issue/view/'.$short; ?>"><?php echo $baseURL.'/issue/view/'.$short; ?></a></p>
 </div>
 <div class="signature">La Rédaction.</div>
--- templates/mail/issue/publication/SLLMOO.php
+++ templates/mail/issue/publication/SLLMOO.php
@@ -1,6 +1,6 @@
 <div class="salutation">Chère lectrice, cher lecteur,</div>
 <div class="content">
-	<p>Une nouvelle archive a été mise en ligne.</p>
+	<p>Un nouveau numéro a été publié.</p>
 	<p>Vous pouvez consulter les actes du colloque "<?php echo $issue['title'] ?? $issue['settings']['title'][$settings['locale']]['value']; ?>" (<?php echo $issue['year']; ?>) en cliquant sur le lien ci-dessous.</p>
 	<p><a href="<?php echo $baseURL.'/issue/view/'.$short; ?>"><?php echo $baseURL.'/issue/view/'.$short; ?></a></p>
 </div>
