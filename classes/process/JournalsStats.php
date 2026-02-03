a585e7ea9eab0cebce0c0cf390adc870ace2bbac
EA <arrive@free.fr>
2026-02-03 16:07:01 +0100

    cosmetic

--- classes/process/JournalsStats.php
+++ classes/process/JournalsStats.php
@@ -21,15 +21,15 @@ class JournalsStats extends ProcessExecutor {
         $counts = [];
         foreach ($queries as $query) {
             $month = date('m', strtotime($query->when));
-            $value = $counts[$query->paper][$query->display][$month] ?? 0;
-            $counts[$query->paper][$query->display][$month] = $value + 1;
+            $value = $counts[$month][$query->paper][$query->display] ?? 0;
+            $counts[$month][$query->paper][$query->display] = $value + 1;
         }
         foreach ((new PaperEntity())->retrieveAll(['journal' => $journal->id]) as $paper) {
             $issue = (new IssueEntity())->retrieveOne($paper->issue);
             $short = JournalsUtils::short($journal->context, $issue->volume, $issue->number, $paper->pages);
             foreach (['01','02','03','04','05','06','07','08','09','10','11','12'] as $month) {
                 foreach (['','html','pdf'] as $display) {
-                    $count = $counts[$short][$display][$month] ?? null;
+                    $count = $counts[$month][$short][$display] ?? null;
                     if (!empty($count)) {
                         $this->info(0, $display."\t".Zord::str_pad($short, 16)."\t".$journal->context."\t".$year.$month."\t".$count."\t".JournalsUtils::short($journal->context, $issue->volume, $issue->number));
                     }
